286793b5e21daa4d3a46c0bac421bd51377b5440
OrtusAfricaCapital <tech@ortusafrica.com>
2021-01-19 22:56:39 +0300

    initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import { Button } from "react-bootstrap";
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Crowdax - 2021</h1>
+        <Button>Fund</Button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
