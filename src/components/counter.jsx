a1a902e6971e6ae4eeaf33b6378a44f557e3b566
OrtusAfricaCapital <tech@ortusafrica.com>
2021-01-19 23:58:58 +0300

    newly updated

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,29 @@ import { Button } from "react-bootstrap";
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 10,
+    imageUrl: "https://picsum.photos/200",
+  };
+
+  styles = {
+    fontSize: 30,
+    fontWeight: "bold",
+  };
+
   render() {
     return (
       <div>
-        <h1>Crowdax - 2021</h1>
-        <Button>Fund</Button>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <Button className="btn btn-secondary btn-sm">funded project (s)</Button>
       </div>
     );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
