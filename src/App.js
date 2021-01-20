0e89f0d38c2aef5e8770490e0ee02955a5196360
OrtusAfricaCapital <tech@ortusafrica.com>
2021-01-21 00:26:02 +0300

    added footer

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import MainNavbar from "./components/navbar/MainNavbar";
 import HomeHeader from "./components/home/HomeHeader";
+import Footer from "./components/footer/Footer";
+
 function App() {
   return (
     <div>
       <MainNavbar />
       <HomeHeader />
+      <Footer />
     </div>
   );
 }
