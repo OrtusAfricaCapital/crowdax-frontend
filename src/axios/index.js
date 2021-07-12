f413b5a9db3a36f5163d83966fa16f45cf55c646
OrtusAfricaCapital <tech@ortusafrica.com>
2021-07-12 05:20:47 +0300

    new baseurl for the api

--- src/axios/index.js
+++ src/axios/index.js
@@ -4,7 +4,7 @@ export default axios.create({
   //baseURL: "http://localhost:8000/api/v1/",
   //headers: { Authorization: "Bearer " + "558ef74464db500e6129ea4843af4a43" },
 
-  baseURL: "http://34.221.241.181/api/v1/",
+  baseURL: "https://chatranslator.com/api/v1/",
   headers: { Authorization: "Bearer " + "3f02a768b44c963ac371e151e3589733" },
 
 });
